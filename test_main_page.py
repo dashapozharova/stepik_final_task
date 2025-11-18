d185cf68c1386f1fef968634faf0ce955be472a6
Dasha <dashapozharova@mail.ru>
2025-11-18 16:33:39 +0500

    Update page navigation

--- test_main_page.py
+++ test_main_page.py
@@ -3,12 +3,14 @@ import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 from pages.main_page import MainPage
+from pages.login_page import LoginPage  # ДОБАВЛЯЕМ импорт LoginPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    login_page = page.go_to_login_page()
+    page.go_to_login_page()  # просто переход без возврата
+    login_page = LoginPage(browser, browser.current_url)  # ЯВНО создаем LoginPage
     login_page.should_be_login_page()
 
 def test_guest_should_see_login_link(browser):
